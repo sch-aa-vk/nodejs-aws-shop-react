645e6a2af5b5c5cdf39218fe2437222f536df0a9
Amina Senchenko <Amina.Senchenko@coxautoinc.com>
2024-06-16 23:19:53 +0500

    feat: add link to products

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -25,7 +25,7 @@ export default function Products() {
           >
             <CardMedia
               sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
+              image="https://images.unsplash.com/photo-1718480419942-a21efc9b5409?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               title="Image title"
             />
             <CardContent sx={{ flexGrow: 1 }}>
