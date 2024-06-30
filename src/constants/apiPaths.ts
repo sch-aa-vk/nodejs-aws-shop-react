b5ef9afb1fd3e86f9c95b1db1a696712e50e0d86
Amina Senchenko <Amina.Senchenko@coxautoinc.com>
2024-06-30 16:04:42 +0500

    feat: change some endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://npfagdphr1.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://8ij07c47u3.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ybnl3zrd61.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
